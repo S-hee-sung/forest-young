cfe350ec2a72ba443da11b738cc96b3badde890a
S-hee-sung <dlsglrhdwjs.naver.com>
2022-12-20 13:27:45 +0900

    12.20 1차 수정

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const headEl = document.querySelector('header');
+const mainMenuEl = document.querySelector('.main-menu');
+const subMenuEl = document.querySelectorAll('.sub-menu')
+
+mainMenuEl.addEventListener('mouseover', function () {
+  headEl.style.height = '400px';
+
+subMenuEl.forEach(function (subMenuEl) {
+    subMenuEl.style.opacity = '1'   
+    subMenuEl.style.visibility = 'visible'   
+  })    
+});
+
+headEl.addEventListener('mouseleave', function () {
+  headEl.style.height = '100px';
+
+  subMenuEl.forEach(function (subMenuEl) {
+    subMenuEl.style.opacity = '0';
+    subMenuEl.style.visibility = 'hidden';
+  });
+})
+
+new Swiper('.slider .swiper', {
+  direction: 'horizontal', 
+  loop: true, 
+  autoplay: true,     
+  navigation: { 
+    nextEl: '.slider .swiper-button-next',
+    prevEl: '.slider .swiper-button-prev',
+    },
+});
